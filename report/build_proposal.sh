fdafe0a526fb49ae0ebb52f1552c0246d024e40a
Rosetta Roberts <robepea2@isu.edu>
2019-05-15 15:06:14 -0600

    made some small changed to generated files

--- report/build_proposal.sh
+++ report/build_proposal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-markdown-pp proposal.mdpp -o proposal.md
-pandoc proposal.md --template=../templates/ieee.latex --top-level-division=section --pdf-engine=xelatex -f markdown+fenced_code_attributes -f markdown+multiline_tables -o proposal.pdf
+markdown-pp proposal.mdpp -o proposal.gen.md
+pandoc proposal.gen.md --template=templates/ieee.latex --top-level-division=section --pdf-engine=xelatex -f markdown+fenced_code_attributes -f markdown+multiline_tables -o proposal.pdf
