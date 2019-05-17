1a651d4d21e3d69c1c7ff8e1d3702713421436c8
Rosetta Roberts <robepea2@isu.edu>
2019-05-16 21:14:23 -0600

    Worked around bug in markdown-pp with including empty files.

--- report/build.sh
+++ report/build.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+set -e
 markdown-pp report.mdpp -e latexrender -o report.gen.md
 
 # indent abstract so it is properly formatted in the abstract.
 sed -e "s/^/    /" < abstract.md > indented-abstract.gen.md
 markdown-pp metadata.yml.mdpp -o metadata.gen.yml
-pandoc report.gen.md --metadata-file=metadata.gen.yml --template=../templates/ieee.latex --top-level-division=section -F pandoc-crossref -F pandoc-citeproc -f markdown+fenced_code_attributes -f markdown+multiline_tables -o report.gen.tex
+pandoc report.gen.md \
+  --metadata-file=metadata.gen.yml \
+  --template=../templates/ieee.latex \
+  --top-level-division=section \
+  -F pandoc-crossref \
+  -F pandoc-citeproc \
+  -f markdown+fenced_code_attributes \
+  -f markdown+multiline_tables \
+  -o report.gen.tex
 xelatex report.gen
 biber report.gen
 xelatex report.gen
