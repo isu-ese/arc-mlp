21d8b36e278bb8421c2b6f98eebea5417bfb4ab5
Rosetta Roberts <robepea2@isu.edu>
2019-05-16 20:12:42 -0600

    Fixed abstract and title

--- report/build.sh
+++ report/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 markdown-pp report.mdpp -e latexrender -o report.gen.md
-pandoc report.gen.md --template=../templates/ieee.latex --top-level-division=section --pdf-engine=xelatex -F pandoc-crossref -F pandoc-citeproc -f markdown+fenced_code_attributes -f markdown+multiline_tables -o report.gen.pdf
+sed -e "s/^/    /" < abstract.md > indented-abstract.gen.md
+markdown-pp metadata.yml.mdpp -o metadata.gen.yml
+pandoc report.gen.md --metadata-file=metadata.gen.yml --template=../templates/ieee.latex --top-level-division=section --pdf-engine=xelatex -F pandoc-crossref -F pandoc-citeproc -f markdown+fenced_code_attributes -f markdown+multiline_tables -o report.gen.pdf
