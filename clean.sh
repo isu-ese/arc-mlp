2fb18037b012e57fb2e25c8bf3048297d4b444db
Rosetta Roberts <robepea2@isu.edu>
2019-05-16 20:33:04 -0600

    now cleans up generated files

---
+++ clean.sh
@@ -0,0 +1,1 @@
+find . -name "*.gen.*" | xargs rm
--- report/build.sh
+++ report/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 markdown-pp report.mdpp -e latexrender -o report.gen.md
+
+# indent abstract so it is properly formatted in the abstract.
 sed -e "s/^/    /" < abstract.md > indented-abstract.gen.md
 markdown-pp metadata.yml.mdpp -o metadata.gen.yml
-pandoc report.gen.md --metadata-file=metadata.gen.yml --template=../templates/ieee.latex --top-level-division=section --pdf-engine=xelatex -F pandoc-crossref -F pandoc-citeproc -f markdown+fenced_code_attributes -f markdown+multiline_tables -o report.gen.pdf
+pandoc report.gen.md --metadata-file=metadata.gen.yml --template=../templates/ieee.latex --top-level-division=section -F pandoc-crossref -F pandoc-citeproc -f markdown+fenced_code_attributes -f markdown+multiline_tables -o report.gen.tex
+xelatex report.gen.tex
