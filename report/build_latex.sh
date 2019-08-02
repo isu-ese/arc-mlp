e80be1c465360e005fc8015d610f0fd8d8f92cf2
Rosetta Roberts <robepea2@isu.edu>
2019-08-02 10:38:57 -0600

    added acknowledgemnets. removed ToC

--- report/build_latex.sh
+++ report/build_latex.sh
@@ -10,8 +10,6 @@ pandoc $1.gen.md \
   --top-level-division=section \
   --bibliography=../isu-ese.json \
   --csl=../ieee.csl \
-  --toc \
-  --toc-depth=2 \
   -F pandoc-crossref \
   -f markdown+fenced_code_attributes+multiline_tables+tex_math_single_backslash+fenced_divs \
   -o $1.gen.tex
--- report/report.md
+++ report/report.md
@@ -2,12 +2,20 @@
 !include(metadata.yml)
 
 !include(introduction/all.md)
+
 !include(background/all.md)
+
 !include(approach/all.md)
+
 !include(methods/all.md)
+
 !include(results/all.md)
+
 !include(discussion/all.md)
+
 !include(conclusion/all.md)
+
+!include(acknowledgements.md)
 
 !bib
 
