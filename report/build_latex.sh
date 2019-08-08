083fe796eddd8417e633d8b3f5fcf58839e9f422
Rosetta Roberts <robepea2@isu.edu>
2019-08-08 16:52:50 -0600

    Fixed accidental space added to build

--- report/build_latex.sh
+++ report/build_latex.sh
@@ -4,9 +4,9 @@ set -e
 
 pp $1.md > $1.gen.md
 
-pandoc $1.gen.md \ 
+pandoc $1.gen.md \
   --template=../assets/templates/ieee.latex \
   --top-level-division=section \
   -F pandoc-crossref -M "crossrefYaml=../assets/crossref/crossrefIEEE.yml" \
   -f markdown+fenced_code_attributes+multiline_tables+tex_math_single_backslash+fenced_divs \
-  -o $1.tex \
+  -o $1.tex
